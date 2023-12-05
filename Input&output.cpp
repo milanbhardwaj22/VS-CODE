c9837e760bf6d551e98e8a897336f547ab26f245
Milan <milanbhardwaj22@gamil.com>
2023-12-05 15:01:09 +0530

    commit

--- Input&output.cpp
+++ Input&output.cpp
@@ -1,1 +1,16 @@
-#inc
+#include<iostream>
+using namespace std;
+void printfunction()
+{
+    int i=1;
+    while (i <=20)
+    {
+        cout << i << " ";
+        i++;
+    }
+}
+int main()
+{
+    printfunction();
+    return 0;
+}
