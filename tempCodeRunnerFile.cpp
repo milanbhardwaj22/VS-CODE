7c29b6dccfb73bb62759edec950adce4552bf17a
Milan <milanbhardwaj22@gamil.com>
2023-12-05 15:00:50 +0530

    commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void printfunction()
+{
+    int i=1;
+    while (i <=20)
+    {
+        cout << i << " ";
+        i++;
+    }
+}
+int main()
+{
+    printfunction();
+    return 0;
+}
