6ab51c4926fbf07f035960027dca6bd6bb5bc2f1
Milan <milanbhardwaj22@gamil.com>
2023-12-06 17:52:11 +0530

    commit

--- loops/tempCodeRunnerFile.cpp
+++ loops/tempCodeRunnerFile.cpp
@@ -1,20 +1,3 @@
-// #include<iostream>
-// using namespace std;
-// int main()
-// {
-//     int n;
-//     cout<<"enter the array size :" ;
-//     cin >>n;
-//     int arr[n];
-//     for(int i=0; i<n; i++)
-//     {
-//         cin>>arr[i];
-//     } 
-//     for(int i=0; i<n; i++)
-//     {cout<<arr[i]<< " ";
-// }
-// }
-
 #include<iostream>
 using namespace std;
 int main()
