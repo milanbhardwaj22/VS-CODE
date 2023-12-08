692941007a34ce99b931cf9639e12e68cadd80c5
Milan <milanbhardwaj22@gamil.com>
2023-12-09 01:10:12 +0530

    commit

--- loops/arrays/tempCodeRunnerFile.cpp
+++ loops/arrays/tempCodeRunnerFile.cpp
@@ -1,24 +1,8 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int arr[]= {25,6,7,8,56,4,23,5,8};
-    int n ;
-    n = sizeof (arr)/sizeof(arr[0]);
-    int max = INT8_MIN;
-
-    int smax = INT8_MIN;
-
-    max = arr[0];
-    for(int i = 0; i<n; i ++)
-    {
-        if(arr[i]>max) max= arr[i];
-    }
-
-    for(int i =0; i<n; i++)
-    {
-        if(arr[i]>smax && smax != max)
-        smax= arr[i];
-    }
-    cout << smax ;
-}
+class student
+ { public: 
+    int marks;     
+    void set_data(int marks){ 
+        this -> marks = marks; 
+    } 
+}; 
+ 
---
+++ loops/vectors.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number \n";
+    cin >> n;
+    // int arr[n] ;
+    vector<int>arr(n);
+    for(int i= 0; i<n; i++ )
+    {
+        cin >> arr[i];
+    }
+     for(int i= 0; i<n; i++ )
+    {
+        cout << arr[i];
+}
+}
+
