4f0c3a57dd8d5f1568a02df30467ceeb3220b2cb
Milan <milanbhardwaj22@gamil.com>
2023-12-10 14:46:30 +0530

    commit

---
+++ Codingc++/OOPS/intro.cpp
@@ -0,0 +1,62 @@
+// #include<iostream>
+// using namespace std;
+// class hero {
+//     char level; 
+//     public :
+//     int health ;
+     
+
+//     void print()
+//     {
+//         cout << level << endl;
+//     }
+//     char getlevel()
+//     {
+//         return level ;
+//     }
+//     void setlevel(char x){
+//         level=x;
+//     }
+// };
+ 
+//  int main()
+// {
+//     hero sharma ;
+//     // cout<< "sharma level is" << sharma.getlevel() << endl;
+//     sharma.health = 50;
+//     sharma.setlevel('A');
+
+//     cout<< "health is " << sharma.health << endl;
+//     cout<< "level is " << sharma.getlevel() << endl;
+    
+//     return 0;
+// }
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class vehicle{
+public:
+    int engine;
+    int horn;
+    vehicle(){
+        cout << "vehicle" << endl;
+    }
+};
+class bike : public vehicle{
+public:
+    int headlight;
+    bike(){
+        cout << "bike" << endl;
+    }
+};
+class car : public vehicle{
+public:
+    int headlight;
+    car(){
+        cout << "car" << endl;
+    }
+};
+int main(){
+car c;
+}
