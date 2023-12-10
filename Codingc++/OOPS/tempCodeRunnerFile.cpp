fe8ba256c5e0bcc9a75bb31d2c75598d16d7f223
Milan <milanbhardwaj22@gamil.com>
2023-12-10 19:12:42 +0530

    commit

---
+++ Codingc++/OOPS/tempCodeRunnerFile.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class rectangle
+{
+public :
+int l ;
+int b;
+rectangle () // default constructor
+{
+    l = 0 ;
+    b = 0;
+}
+rectangle(int x, int y){ // parameterised constuctors
+    l = 5;
+    b = 6;
+}
+ rectangle(rectangle& r  ){
+l = r.l;
+b = r.b;
+ }
+};
+
+
+int main()
+{
+rectangle r1;
+cout << r1.l << " " << r1.b << endl;
+
+rectangle r2;
+cout<< r2.l << " " << r2.b << endl;
+
+rectangle r3=r2;
+cout << r3.l << " " << r3.b << endl;
+    return 0;
+}
