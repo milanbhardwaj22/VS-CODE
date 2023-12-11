46f998d24867e7aa2a1618edc21a4bd442899092
Milan <milanbhardwaj22@gamil.com>
2023-12-12 02:10:22 +0530

    commit

--- Codingc++/practice.cpp
+++ Codingc++/practice.cpp
@@ -57,33 +57,70 @@
 //         }
 //     }
 // }
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//    int row , col;
+//    cin >> row ;
+//    cin >> col ;
+//    int arr[row ][col];
+//    for (int i =0 ; i<row; i++)
+//    {
+//     for (int j =0 ; j<col; j++)
+//     {
+//         cin >> arr[i][j];
+//     }
+//    }
+//    for(int i =0; i<row; i++)
+//    {
+//     if(i %2!=0)
+//     {
+//         for(int j=col-1; j>=0; j--)
+//         {
+//             cout << arr[i][j]<<" ";
+//         }
+//     }
+//     else{
+//         for (int j=0; j<col; j++)
+//         {
+//             cout << arr[i][j]<< " ";
+//     }
+// }
+// }
+// }
+
+#include<iostream>
 using namespace std;
-int main(){
-   int row , col;
-   cin >> row ;
-   cin >> col ;
-   int arr[row ][col];
-   for (int i =0 ; i<row; i++)
-   {
-    for (int j =0 ; j<col; j++)
+int main()
+{
+    int row , col ;
+    cin >> row ;
+    cin >> col;
+    int arr[row][col];
+    for (int i=0; i < row; i ++)
     {
-        cin >> arr[i][j];
-    }
-   }
-   for(int i =0; i<row; i++)
-   {
-    if(i %2!=0)
-    {
-        for(int j=col-1; j>=0; j--)
+        for(int j=0; j<col ; j++)
         {
-            cout << arr[i][j]<<" ";
+            cin >> arr[i][j];
         }
     }
-    else{
-        for (int j=0; j<col; j++)
+    //1 2 3     1 2 3
+    //4 5 6     6 5 4
+    //7 8 9     7 8 9 
+    for (int i =0; i<row; i++)
+    {
+        if(i%2 !=0)
         {
-            cout << arr[i][j]<< " ";
-        }
-    }
-   }
+            for(int j=col-1; j>=0; j-- )
+            {
+                cout << arr[i][j];
+            }
+        }
+        else{
+            for(int j=0; j<col ; j++)
+        {
+            cout << arr[i][j];
+        }
+        }
+    }
+}
