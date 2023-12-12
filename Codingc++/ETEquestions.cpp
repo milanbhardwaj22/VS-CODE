378698b323049ccb3deffcf2b8ff47c720ebe6e7
Milan <milanbhardwaj22@gamil.com>
2023-12-12 11:48:49 +0530

    commit

--- Codingc++/ETEquestions.cpp
+++ Codingc++/ETEquestions.cpp
@@ -423,3 +423,36 @@
 //     }
 //     cout << segments << endl;
 // }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row , col ;
+      cin >> row , col;
+    int arr[row][col];
+    for(int i= 0; i<row; i++)
+    {
+        for(int j=0; j<col; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    for(int i= 0; i<row; i++)
+    {
+        if(i%2 !=0)
+        {
+            for(int j=col-1; j<0; j--)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+        else
+        {
+            for(int j=0; j<col; j++)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+    }
+}
--- loops/variablesizearray.cpp
+++ loops/variablesizearray.cpp
@@ -38,30 +38,30 @@
 //     else cout << h << "not present";
 // }
 
-// #include<iostream>
-// using namespace std;
-// int main()
-// {
-//     int arr[] = {3,4,5,6,7,4,56,5,44};
-//     int n = sizeof (arr)/sizeof (arr[0]);
-//     int x;
-//     cout << "Enter the targetted number:";
-//     cin >> x;
-//     bool flag = false ;
-//     int i;
-//     for(i=0; i<x; i++)
-//     {
-//         if(arr[i] == x)
-//         {
-//             flag = true;
-//         }
-//     }
-//     if(flag == true )
-//     {
-//         cout << x << "number is present ";
-//     }
-//     else{
-//         cout << x << "number is not present";
-//     }
-// }
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {3,4,5,6,7,4,56,5,44};
+    int n = sizeof (arr)/sizeof (arr[0]);
+    int x;
+    cout << "Enter the targetted number:";
+    cin >> x;
+    bool flag = false ;
+    int i;
+    for(i=0; i<x; i++)
+    {
+        if(arr[i] == x)
+        {
+            flag = true;
+        }
+    }
+    if(flag == true )
+    {
+        cout << x << "number is present ";
+    }
+    else{
+        cout << x << "number is not present";
+    }
+}
 
