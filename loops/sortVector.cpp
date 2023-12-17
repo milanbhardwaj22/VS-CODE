659cb7619f326b7b95fd2adb85f2a2796e084d17
Milan <milanbhardwaj22@gamil.com>
2023-12-17 22:29:27 +0530

    commit

--- loops/sortVector.cpp
+++ loops/sortVector.cpp
@@ -1,23 +1,23 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-int main()
-{
-    vector<int>v(5);
-    for(int i =0; i<v.size() ; i++)
-    {
-        cin >>  v[i];
-    }
-    for(int i =0; i<v.size() ; i++)
-    {
-        cout << v[i];
-    }
-    cout << endl;
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main()
+// {
+//     vector<int>v(5);
+//     for(int i =0; i<v.size() ; i++)
+//     {
+//         cin >>  v[i];
+//     }
+//     for(int i =0; i<v.size() ; i++)
+//     {
+//         cout << v[i];
+//     }
+//     cout << endl;
 
-    int sort ( int v.begin() , v.end() );
-    for(int i =0; i<v.size() ; i++)
-    {
-        cout << v[i];
-    }
-}
+//     int sort ( int v.begin() , v.end() );
+//     for(int i =0; i<v.size() ; i++)
+//     {
+//         cout << v[i];
+//     }
+// }
 
