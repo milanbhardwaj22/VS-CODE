77501ee891d9367a3999f1383741da1c4df91d02
Milan <milanbhardwaj22@gamil.com>
2023-12-17 22:29:04 +0530

    commit

--- Codingc++/OOPS/constructors.cpp
+++ Codingc++/OOPS/constructors.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 class rectangle
 {
-
 public :
 int l ;
 int b;
@@ -20,10 +19,7 @@ l = r.l;
 b = r.b;
  }
 };
-
-
 int main()
-
 {
 rectangle r1;
 cout << r1.l << " " << r1.b << endl;
