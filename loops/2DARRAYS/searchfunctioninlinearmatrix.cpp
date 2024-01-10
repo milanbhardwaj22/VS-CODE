a8c7c570e3436f8c601f115605eaa22a8110879c
Milan <milanbhardwaj22@gamil.com>
2024-01-10 17:47:02 +0530

    gi

--- loops/2DARRAYS/searchfunctioninlinearmatrix.cpp
+++ loops/2DARRAYS/searchfunctioninlinearmatrix.cpp
@@ -1,48 +1,60 @@
-#include<iostream>
-using namespace std;
-bool ispresent(int arr[3][4] , int target, int row, int col )  
-{
-    for(int row=0; row<3; row++)
-    {
-        for(int col=0; col<3; col++)
-        {
-         if (arr[row][col] == target)
-         {
-            return 1;
-         }
-        }
-    }
-    return 0;
-}
-    int main()
-    {
-    int arr[3][4];
-    for(int row=0; row<3; row++)
-    {
-        for(int col=0; col<3; col++)
-        {
-         cin >> arr[row][col];
-        }
-    }
+// #include<iostream>
+// using namespace std;
+// bool ispresent(int arr[3][4] , int target, int row, int col )  
+// {
+//     for(int row=0; row<3; row++)
+//     {
+//         for(int col=0; col<3; col++)
+//         {
+//          if (arr[row][col] == target)
+//          {
+//             return 1;
+//          }
+//         }
+//     }
+//     return 0;
+// }
+//     int main()
+//     {
+//     int arr[3][4];
+//     for(int row=0; row<3; row++)
+//     {
+//         for(int col=0; col<3; col++)
+//         {
+//          cin >> arr[row][col];
+//         }
+//     }
 
-    for(int row=0; row<3; row++)
-    {
-        for(int col=0; col<3; col++)
-        {
-         cout << arr[row][col]  << " ";
-        }
-        cout << endl;
-    }
+//     for(int row=0; row<3; row++)
+//     {
+//         for(int col=0; col<3; col++)
+//         {
+//          cout << arr[row][col]  << " ";
+//         }
+//         cout << endl;
+//     }
      
-     cout<< "enter the number to be searched" << endl;
-     int target ;
-     cin >> target ;
-     if(ispresent (arr,target,3,3))
-     {
-        cout<<"element is present" << endl;
-     }
-     else{
-        cout<< "element is not present" <<endl;
-     }
-    }
+//      cout<< "enter the number to be searched" << endl;
+//      int target ;
+//      cin >> target ;
+//      if(ispresent (arr,target,3,3))
+//      {
+//         cout<<"element is present" << endl;
+//      }
+//      else{
+//         cout<< "element is not present" <<endl;
+//      }
+//     }
 
+
+#include <iostream>
+using namespace std;
+int main (){
+   int arr[] = { 2, 5, 7, 8, 2, 6, 9 };
+   int n = 7, sum = 0;
+   for(int i = 0; i<n ; i++){
+      sum+=arr[i];
+   }
+   cout<<"The array sum is "<<sum;
+   return 0;
+}
--- loops/2DARRAYS/waveprint.cpp
+++ loops/2DARRAYS/waveprint.cpp
@@ -1,90 +1,46 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int r , c;
-    cin >> r >> c;
-    int arr[r][c];
-=======
+
 // #include<iostream>
 // using namespace std;
 // int main()
 // {
-//     int r , c;
-
+//     int r, c;
 //     cin >> r >> c;
-//     int arr[r][c];
-
-    for(int i= 0;  i<r; i++)
-    {
-        for(int  j=0 ; j<c; j++)
-        {
-            cin >> arr[i][j];
-        }
-    }
-    for(int j= 0; j<c; j++)
-    {
-    if(j %2 == 0)
-    {
-        for( int i=0 ; i<r; i++)
-        {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-    else
-    {
-        for(int i =r-1; i>=0; i--)
-        {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-    }
-}
-
-#include<iostream>
-using namespace std;
-int main()
-{
-    int r, c;
-    cin >> r >> c;
-    int arr1[r][c];
-    int arr2[r][c];
-    for(int i= 0;  i<r; i++)
-    {
-        for(int  j=0 ; j<c; j++)
-        {
-            cin >> arr1[i][j];
-        }
-}
-for(int i= 0;  i<r; i++)
-    {
-        for(int  j=0 ; j<c; j++)
-        {
-            cin >> arr2[i][j];
-        }
-}
-int ans[r][c];
-for (int i= 0 ; i<r; i++)
-{
-    for(int j =0; j<c; j++)
-    {
-        ans[i][j]=0;
-        for(int k=0 ; k<c; k++)
-        {
-            ans[i][j] += arr1[i][k] * arr2[k][j];
-        }
-    }
-}
-for(int i =0; i<r; i++)
-{
-    for(int j =0; j<c; j++)
-    {
-        cout << ans[i][j] << " ";
-    }
-}
-}
+//     int arr1[r][c];
+//     int arr2[r][c];
+//     for(int i= 0;  i<r; i++)
+//     {
+//         for(int  j=0 ; j<c; j++)
+//         {
+//             cin >> arr1[i][j];
+//         }
+// }
+// for(int i= 0;  i<r; i++)
+//     {
+//         for(int  j=0 ; j<c; j++)
+//         {
+//             cin >> arr2[i][j];
+//         }
+// }
+// int ans[r][c];
+// for (int i= 0 ; i<r; i++)
+// {
+//     for(int j =0; j<c; j++)
+//     {
+//         ans[i][j]=0;
+//         for(int k=0 ; k<c; k++)
+//         {
+//             ans[i][j] += arr1[i][k] * arr2[k][j];
+//         }
+//     }
+// }
+// for(int i =0; i<r; i++)
+// {
+//     for(int j =0; j<c; j++)
+//     {
+//         cout << ans[i][j] << " ";
+//     }
+// }
+// }
  
 
  //unique element
