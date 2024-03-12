3d590e29b0a2ed18652b19b1cb3eb1cd7857e439
Milan <milanbhardwaj22@gamil.com>
2024-03-12 17:46:03 +0530

    commit

--- HTML/fee/practice.js
+++ HTML/fee/practice.js
@@ -1,14 +1,6 @@
-// arr=[13,58,23,45,26,48,100,66];
-// arr2 = [];
-// arr.sort(function(a,b){return a-b});
-// for(let i=0,ni=0;i<arr.length;i++){
-//     if(arr[i]%2==0){
-//         arr2[ni] = arr[i];
-//         console.log(arr[i],"<br>");
-//     }
-// }
-
-
-// for(let i=0;i<arr2.length;i++){
-//     console.log(arr2[i],"<br>");
-// }
+<script>
+let x = 50;
+let y = 25;
+let sum = x + y;
+console.log(sum);
+</script>
