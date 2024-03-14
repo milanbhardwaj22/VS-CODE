6d69d34dff6b96299b681c8d39480396707fdc24
Milan <milanbhardwaj22@gamil.com>
2024-03-14 17:59:15 +0530

    commit

---
+++ Codingc++/UDEMY.cpp/ArrayRevision.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the size";
+    cin >> n;
+    int A[n];
+    A[0]= 2;
+
+    for(int x:A)
+    {
+        cout << x << endl;
+    }
+}
