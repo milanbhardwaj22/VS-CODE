f81aaf3e1c1dcf670351f9cb0112d0d11d6f4e64
Milan <milanbhardwaj22@gamil.com>
2024-03-18 12:06:54 +0530

    commit

---
+++ Codingc++/UDEMY.cpp/structureRevision.cpp
@@ -0,0 +1,37 @@
+// #include<stdio.h>
+
+// #include<iostream>
+// using namespace std;
+
+// struct Rectangle
+// {
+//     int length;
+//     int breadth;
+//     double x;
+// };
+
+
+// int main()
+// {
+// struct Rectangle r1 = {10,5};
+
+// r1.length=15;
+// r1.breadth = 43;
+// cout <<r1.length<< endl;
+// cout <<r1.breadth<< endl;
+
+// return 0;
+// }
+
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a =10;
+    int *P;
+    P = &a;
+    printf("%d" , a);
+    cout << endl;
+    printf("%d" , *P);
+}
