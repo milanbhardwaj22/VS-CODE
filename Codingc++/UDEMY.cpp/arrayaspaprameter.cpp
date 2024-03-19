32886a5ae3ed6dc257fa75f8ef72104f5626a038
Milan <milanbhardwaj22@gamil.com>
2024-03-19 19:38:14 +0530

    commit

--- Codingc++/UDEMY.cpp/arrayaspaprameter.cpp
+++ Codingc++/UDEMY.cpp/arrayaspaprameter.cpp
@@ -1,15 +1,26 @@
 #include<iostream>
 #include<stdio.h>
 using namespace std;
-void fun(int *A, int n)
-{
-int i;
-for(i=0; i<n; i++)
-printf("%d" , A[i]);
-}
+// void fun(int *A, int n)
+// {
+// int i;
+// for(i=0; i<n; i++)
+// printf("%d" , A[i]);
+// }
 
-int main()
+// int main()
+// {
+//     int A[5] = {2,3,4,5,6};
+//     fun(A,5);
+void fun(int A[])
 {
-    int A[5] = {2,3,4,5,6};
-    fun(A,5);
+
+}
+int  main()
+{
+    int A[]= {2,4,5,6,7};
+    int n = 6;
+    for(int x:A)
+    cout << x << endl;
+    return 0;
 }
