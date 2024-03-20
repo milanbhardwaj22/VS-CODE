dbf1368c0d4cd5f31b7e99cc286eb904cc5450a9
Milan <milanbhardwaj22@gamil.com>
2024-03-20 10:54:19 +0530

    commit

---
+++ Codingc++/UDEMY.cpp/parametersasarray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int * fun(int size)  // *represent its a pointer , fun is a function
+{
+    int *p ; //*p is a pointer
+    p = new int[size]; //array is assigned to a pointer
+
+    for(int i=0; i<size ; i++)
+    p[i] = i+1; // +2 is used for proper memory allocation
+    return p;
+}
+int main()
+{
+    int * ptr , size =7;
+    ptr = fun(size);
+    for (int i=0; i<size; i++)
+    cout << ptr[i] << endl;
+    return 0;
+
+}
