de10016e104a399e579bd3da88e26d3c0061f277
Milan <milanbhardwaj22@gamil.com>
2024-04-02 10:07:12 +0530

    commit

---
+++ Codingc++/UDEMY.cpp/structureasparameters.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+struct rectangle 
+{
+    int length ;
+    int breadth ;
+};
+
+void fun(struct rectangle r)
+{
+    r.length = 45;
+    cout << "length" << r.length << endl<< "breadth" <<r.breadth <<endl;
+}
+
+int main()
+{
+    struct rectangle r = {10,5};
+    fun(r);
+    printf("length %d \n breadth %d \n"  , r.length , r.breadth );
+    return 0;
+}
