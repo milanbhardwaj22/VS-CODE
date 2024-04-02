48656e9c674edfe74d044d0e3031f4e7f627c31a
Milan <milanbhardwaj22@gamil.com>
2024-04-02 19:37:14 +0530

    commit

--- Codingc++/UDEMY.cpp/structureasparameters.cpp
+++ Codingc++/UDEMY.cpp/structureasparameters.cpp
@@ -22,26 +22,26 @@ using namespace std;
 //     return 0;
 // }
 
-struct rectangle 
-{
-    int length ;
-    int breadth;
-};
-void initialize(struct rectangle *r,int l , int b)
-{
-r -> length = l;
-r -> breadth = b;
-}
-int area(struct rectangle r)
-{
-    r.length = 45;
-    cout << "length" << r.length << endl<< "breadth" <<r.breadth <<endl;
-}
+// struct rectangle 
+// {
+//     int length ;
+//     int breadth;
+// };
+// void initialize(struct rectangle *r,int l , int b)
+// {
+// r -> length = l;
+// r -> breadth = b;
+// }
+// int area(struct rectangle r)
+// {
+//     r.length = 45;
+//     cout << "length" << r.length << endl<< "breadth" <<r.breadth <<endl;
+// }
 
-int main()
-{
-    struct rectangle r = {10,5};
-    fun(r);
-    printf("length %d \n breadth %d \n"  , r.length , r.breadth );
-    return 0;
-}
+// int main()
+// {
+//     struct rectangle r = {10,5};
+//     fun(r);
+//     printf("length %d \n breadth %d \n"  , r.length , r.breadth );
+//     return 0;
+// }
