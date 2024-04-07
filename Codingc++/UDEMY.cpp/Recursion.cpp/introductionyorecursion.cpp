77d9097e95ce327703f0ea5e8fb813a775fb27e3
Milan <milanbhardwaj22@gamil.com>
2024-04-07 18:37:05 +0530

    commit

--- Codingc++/UDEMY.cpp/Recursion.cpp/introductionyorecursion.cpp
+++ Codingc++/UDEMY.cpp/Recursion.cpp/introductionyorecursion.cpp
@@ -15,18 +15,34 @@
 // return 0;
 // }
 
+// #include<stdio.h>
+// void fun(int n )
+// {
+//     if(n>0)
+//     {
+//         fun(n-1);
+//         printf("%d \n" , n);
+//     } 
+// }
+// int main()
+// {
+//     int x =3;
+//     fun(x);
+//     return 0;
+// }
+
 #include<stdio.h>
-void fun(int n )
+int fun(int n)
 {
     if(n>0)
     {
-        fun(n-1);
-        printf("%d \n" , n);
-    } 
+        return fun(n-1)+n;
+    }
+    return 0;
 }
+
 int main()
 {
-    int x =3;
-    fun(x);
-    return 0;
+    int a =5;
+    printf("%d " , fun(a));
 }
