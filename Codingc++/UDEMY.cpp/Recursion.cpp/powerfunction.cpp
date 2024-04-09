fb6e3a08fda50c2d5bf0de03e233ee2b3d9da525
Milan <milanbhardwaj22@gamil.com>
2024-04-09 17:49:59 +0530

    commit

--- Codingc++/UDEMY.cpp/Recursion.cpp/powerfunction.cpp
+++ Codingc++/UDEMY.cpp/Recursion.cpp/powerfunction.cpp
@@ -1,15 +1,17 @@
 #include<stdio.h>
-int power(int n , int m)
+int power(int m , int n)
 {
-    if(n == 0)
+    if(n == 0 )  
     return 1;
-    if( n %2 == 0)
-    return power(m*m, n/2);
-    return m * power(m*m , (n-1)/2);
+    if( n%2 == 0)
+    return power(m*m  ,n /2);  // if the condition if even .
+    return m*power(m*m , (n-1)/2); // if the condition is odd.
 }
 
 int main()
 {
-    int r = power(2,4);
+    int r; // variable is declared.
+    r = power(2,4); // used for assising the values.
     printf("%d" , r);
+
 }
